ec51624a52447ca63d81d297fc96c6be5c40316f
Martin Georgiev <Martin.Georgiev@fourth.com>
2021-12-07 10:33:16 +0200

    Mobile handled

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,21 +1,21 @@
-import { Title, Link } from "@ui5/webcomponents-react";
+import { Label, Link } from "@ui5/webcomponents-react";
 import "./Education.css";
 
 function Education() {
   return (
     <div>
-      <Title level="H1"><b>Education</b></Title>
+      <h1 className="resume__education-title">Education</h1>
       <br/>
       <br/>
-      <Title level="H3" className="resume__education-label">
+      <p level="H3" className="resume__education-label">
         <b>School: </b>High School of Computers & Technologies - Pravets
-      </Title>
-      <Title level="H3" className="resume__education-label">
+      </p>
+      <p level="H3" className="resume__education-label">
         <b>University: </b>Sofia University - B.S. Textile Technology and Engineering
-      </Title>
-      <Title level="H3" className="resume__education-label">
+      </p>
+      <p level="H3" className="resume__education-label">
         <b>SoftUni: </b>JS Web Developer <Link className="resume__education-link" target="_blank" href="https://softuni.bg/certificates/details/51087/2917249b">Diploma</Link>
-      </Title>
+      </p>
     </div>
   );
 }
